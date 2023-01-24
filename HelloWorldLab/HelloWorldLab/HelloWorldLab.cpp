630abc690ab3abad1e00e155fb6534fad6ae755e
sammcdo <sam_mcdo@yahoo.com>
2023-01-24 15:06:15 -0500

    hello world lab

---
+++ HelloWorldLab/HelloWorldLab/HelloWorldLab.cpp
@@ -0,0 +1,12 @@
+// Sam McDowell
+// HelloWorldLab.cpp : This program prints hello world to the user
+// CSCN 111
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
