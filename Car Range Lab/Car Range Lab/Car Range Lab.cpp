f1fd51c9da9a57c21d28a7641353a3914470e5da
sammcdo <sam_mcdo@yahoo.com>
2023-01-26 10:27:23 -0500

    Lab 2: Car Range

---
+++ Car Range Lab/Car Range Lab/Car Range Lab.cpp
@@ -0,0 +1,36 @@
+﻿// Sam McDowell
+// Car Range Lab.cpp : This program check the range of a car based on mpg
+// CSCN 111
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tankCapacity;
+    int milesPerGallon;
+    int range;
+
+    cout << "Sam's Fuel Calculator!!\n\n";
+
+
+    cout << "Fuel Efficiency (MPG): ";
+    cin >> milesPerGallon;
+    cout << "You entered: " << milesPerGallon << "\n\n";
+
+
+    cout << "Tank Capacity (Gal): ";
+    cin >> tankCapacity;
+    cout << "You entered: " << tankCapacity << endl;
+
+
+    range = milesPerGallon * tankCapacity;
+    
+    
+    cout << "\n\n";
+    cout << "Total Range: ";
+    cout << range << " miles!" << endl;
+
+    return 0;
+}
